fed051694b649cd26cb6f740bb356579e66fbf52
quet-romain <Quet.romain@gmail.com>
2016-09-27 16:58:59 +0200

    update

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-/* votre fonction ici*/
+
+var paragraphe = document.getElementById('parag1');
+
+function changerStyle(){
+	paragraphe.style.backgroundColor = "#000";
+	paragraphe.style.color = "#fff";
+	paragraphe.style.border = "10px dotted red";
+}
